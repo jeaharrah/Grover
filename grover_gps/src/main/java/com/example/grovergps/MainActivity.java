bb11b62f5b34c8ff1e20b75dc659bb9bb67610bc
Jennifer A'Harrah <jeaharrah@gmail.com>
2018-09-15 20:44:27 -0400

    Set the navigation icon in the toolbar as the white crosshair icon that represents the GPS app. This is simply for aesthetic purposes and the pressed icon does not have any functionality.

--- grover_gps/src/main/java/com/example/grovergps/MainActivity.java
+++ grover_gps/src/main/java/com/example/grovergps/MainActivity.java
@@ -100,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.gps_crosshair);
         ButterKnife.bind(this);
 
         // initialize the necessary libraries
