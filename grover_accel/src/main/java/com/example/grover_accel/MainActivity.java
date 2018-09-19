c9c6a258726749642e06a41ddf3c3f9914db2818
Jennifer A'Harrah <jeaharrah@gmail.com>
2018-09-19 08:41:14 -0400

    Added accelerometer logic to Main Activity of GPS accelerometer.

--- grover_accel/src/main/java/com/example/grover_accel/MainActivity.java
+++ grover_accel/src/main/java/com/example/grover_accel/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.grover_accel;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+ * Penn State Abington
+ * Grover ("Ground-based Rover") Project
+ * Accelerometer App
+ * Retrieves accelerometer data from the phone attached to the Grover
+ * Code taken from tutorial at: https://code.tutsplus.com/tutorials/using-the-accelerometer-on-android--mobile-22125
+ */
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+
+
+    /**
+     * @author Jennifer A'Harrah (jka5240)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,15 +35,79 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager
+                .SENSOR_DELAY_NORMAL);
+
     }
+
+
+    // Private fields for shake gesture recognition
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+
+    /**
+     * @author Jennifer A'Harrah (jka5240)
+     * @param sensorEvent
+     */
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            // Stores the current time to the millisecond
+            long currentTime = System.currentTimeMillis();
+
+            // Check to see if 100 milliseconds have passed since the last time the
+            // onSensorChanged method was invoked
+            if ((currentTime - lastUpdate) > 100) {
+                long diffTime = (currentTime - lastUpdate);
+                lastUpdate = currentTime;
+
+                //Perform some math calculations to determine the device's speed
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+                // Statically declare SHAKE_THRESHOLD variable to see whether a shake gesture
+                // has been detected
+                if (speed > SHAKE_THRESHOLD) {
+
+                }
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+
+            }
+        }
+    }
+
+    /**
+     *
+     * @param sensor - the device sensor
+     * @param accuracy - the integer accuracy of the sensor
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager
+                .SENSOR_DELAY_NORMAL);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -53,13 +131,4 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
 }
