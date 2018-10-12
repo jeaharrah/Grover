2ca719b7460e6676cdf405e49e7040ef24ec3b27
Jennifer A'Harrah <jeaharrah@gmail.com>
2018-10-12 13:13:50 -0400

    Finished logic in Date/Time app for retrieving the date and converting it to Julian and displaying it in the TextViews.

--- grover_datetime/src/main/java/com/example/grover_datetime/MainActivity.java
+++ grover_datetime/src/main/java/com/example/grover_datetime/MainActivity.java
@@ -8,8 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 // Imports for Calendar and Date
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Penn State Abington Grover ("Ground-based Rover") Project - Date/Time App.
@@ -33,18 +35,57 @@ public class MainActivity extends AppCompatActivity {
         TextView timeTxtView = (TextView) findViewById(R.id.timeTxtView);
         timeTxtView.setText(currentTime.toString());
 
+        int julian = convertToJulian(currentTime);
+        TextView julianTxtView = (TextView) findViewById(R.id.julianTxtView);
+        julianTxtView.setText(String.valueOf(julian));
+
     }
 
     /**
      * Update the date/time when the XML button is pressed
-     * @param view - so that the XML button can use this method in the onClick attribute
+     * @return the date object for use in the convertToJulian method
      * @author Jennifer A'Harrah (jka5240)
      */
-    public void getDate(View view) {
-
+    public Date getDate() {
         Date currentTime = Calendar.getInstance().getTime();
+        return currentTime;
+    }
+
+    /**
+     * This method converts a given calendar date into Julian
+     * @param date
+     * @return the integer representation of the Julian date
+     * @author Jennifer A'Harrah (jka5240)
+     */
+    public int convertToJulian(Date date){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        String syear = String.format(Locale.US,"%04d",year).substring(2);
+        int century = Integer.parseInt(String.valueOf(((year / 100)+1)).substring(1));
+        int julian = Integer.parseInt(String.format(Locale.US, "%d%s%03d",century,syear,calendar.get
+                (Calendar.DAY_OF_YEAR)));
+
+        TextView julianTxtView = (TextView) findViewById(R.id.julianTxtView);
+        julianTxtView.setText(String.valueOf(julian));
+
+        return julian;
+    }
+
+    /**
+     * This method updates the date/time and Julian values displayed in the TextViews
+     * @param view - so that the XML button can use this method in the onClick attribute
+     * @author Jennifer A'Harrah jka5240
+     */
+    public void displayDateInfo(View view) {
+        Date date = getDate();
+        int julian = convertToJulian(date);
+
         TextView timeTxtView = (TextView) findViewById(R.id.timeTxtView);
-        timeTxtView.setText(currentTime.toString());
+        timeTxtView.setText(date.toString());
+
+        TextView julianTxtView = (TextView) findViewById(R.id.julianTxtView);
+        julianTxtView.setText(String.valueOf(julian));
 
     }
 
