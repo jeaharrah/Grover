9a09f3ad420646061b3d876b8d89a1a5c31e58a1
Jennifer A'Harrah <jeaharrah@gmail.com>
2018-11-17 20:50:24 -0500

    Added SQLLite logic and SQL calls to the main Grover module. Modified layout of main content XML file for main module to better work with these SQL calls. (Note: This SQL code will not stay in the main module. This is its temporary home until I can get it working, at which point I will integrate it into each of the actual apps/modules that is collecting data).

--- app/src/main/java/com/example/jeaha/grovermodule/MainActivity.java
+++ app/src/main/java/com/example/jeaha/grovermodule/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.jeaha.grovermodule;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import android.database.Cursor;
 import android.database.sqlite.*;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // Database name
@@ -24,12 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
     // Table Columns Names
     private static final String FLASHLIGHT_KEY_ID = "FlashlightID";
+    private static final String FLASHLIGHT_NAME = "FlashlightName";
     private static final String FLASHLIGHT_STATE = "FlashlightState";
 
     private static final String GPS_KEY_ID = "GpsID";
     private static final String GPS_NAME = "GpsName";
 
     private static final String ACCELEROMETER_KEY_ID = "AccelerometerID";
+    private static final String ACCELEROMETER_NAME = "AccelerometerName";
     private static final String ACCELEROMETER_X_NAME = "AccelerometerXName";
     private static final String ACCELEROMETER_Y_NAME = "AccelerometerYName";
     private static final String ACCELEROMETER_Z_NAME = "AccelerometerZName";
@@ -48,7 +54,88 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Retrieve the GridView resources
+        GridView gridF = (GridView) findViewById(R.id.gridViewFlashlight);
+        gridF.setBackgroundColor(Color.BLUE);
+        GridView gridG = (GridView) findViewById(R.id.gridViewGps);
+        gridG.setBackgroundColor(Color.RED);
+        GridView gridA = (GridView) findViewById(R.id.gridViewAccel);
+        gridA.setBackgroundColor(Color.GREEN);
+        GridView gridT = (GridView) findViewById(R.id.gridViewTemp);
+        gridT.setBackgroundColor(Color.YELLOW);
 
+        try {
+            // MODE_PRIVATE = only this application
+            db = openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
+
+            // Remove the table and start fresh
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_FLASHLIGHT);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_GPS);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_ACCELEROMETER);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_TEMPERATURE);
+
+            db.execSQL(" CREATE TABLE IF NOT EXISTS " + TABLE_FLASHLIGHT + "(" + FLASHLIGHT_KEY_ID
+                    + " INTEGER PRIMARY KEY AUTOINCREMENT, " + FLASHLIGHT_NAME + " VARCHAR);");
+
+            db.execSQL(" CREATE TABLE IF NOT EXISTS " + TABLE_GPS + "(" + GPS_KEY_ID
+                    + " INTEGER PRIMARY KEY AUTOINCREMENT, " + GPS_NAME + " VARCHAR);");
+
+            db.execSQL(" CREATE TABLE IF NOT EXISTS " + TABLE_ACCELEROMETER + "(" +
+                    ACCELEROMETER_KEY_ID
+                    + " INTEGER PRIMARY KEY AUTOINCREMENT, " + ACCELEROMETER_NAME + " VARCHAR);");
+            db.execSQL(" CREATE TABLE IF NOT EXISTS " + TABLE_ACCELEROMETER + "(" +
+                    ACCELEROMETER_KEY_ID
+                    + " INTEGER PRIMARY KEY AUTOINCREMENT, " + ACCELEROMETER_X_NAME + " VARCHAR);");
+            db.execSQL(" CREATE TABLE IF NOT EXISTS " + TABLE_ACCELEROMETER + "(" +
+                    ACCELEROMETER_KEY_ID
+                    + " INTEGER PRIMARY KEY AUTOINCREMENT, " + ACCELEROMETER_Y_NAME + " VARCHAR);");
+            db.execSQL(" CREATE TABLE IF NOT EXISTS " + TABLE_ACCELEROMETER + "(" +
+                    ACCELEROMETER_KEY_ID
+                    + " INTEGER PRIMARY KEY AUTOINCREMENT, " + ACCELEROMETER_Z_NAME + " VARCHAR);");
+
+            db.execSQL(" CREATE TABLE IF NOT EXISTS " + TABLE_TEMPERATURE + "(" + TEMPERATURE_KEY_ID
+                    + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TEMPERATURE_NAME + " VARCHAR);");
+
+            db.execSQL(" CREATE TABLE IF NOT EXISTS " + TABLE_TEMPERATURE + "(" + TEMPERATURE_KEY_ID
+                    + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TEMPERATURE_UNIT + " VARCHAR);");
+
+            db.execSQL("INSERT INTO " + TABLE_FLASHLIGHT + "(" + FLASHLIGHT_NAME + ") VALUES " +
+                    "('ON');");
+            db.execSQL("INSERT INTO " + TABLE_FLASHLIGHT + "(" + FLASHLIGHT_NAME + ") VALUES " +
+                    "('OFF');");
+            db.execSQL("INSERT INTO " + TABLE_GPS + "(" + GPS_NAME + ") VALUES " +
+                    "('-36.5, 125.7');");
+            db.execSQL("INSERT INTO " + TABLE_ACCELEROMETER + "(" + ACCELEROMETER_NAME + ") " +
+                    "VALUES " +
+                    "('(X) -5, (Y) 7, (Z) -9');");
+            db.execSQL("INSERT INTO " + TABLE_ACCELEROMETER + "(" + ACCELEROMETER_X_NAME + ") " +
+                    "VALUES " +
+                    "('1');");
+            db.execSQL("INSERT INTO " + TABLE_ACCELEROMETER + "(" + ACCELEROMETER_Y_NAME + ") " +
+                    "VALUES " +
+                    "('2');");
+            db.execSQL("INSERT INTO " + TABLE_ACCELEROMETER + "(" + ACCELEROMETER_Z_NAME + ") " +
+                    "VALUES " +
+                    "('3');");
+            db.execSQL("INSERT INTO " + TABLE_TEMPERATURE + "(" + TEMPERATURE_UNIT + ") " +
+                    "VALUES " +
+                    "('Fahrenheit');");
+            db.execSQL("INSERT INTO " + TABLE_TEMPERATURE + "(" + TEMPERATURE_UNIT + ") " +
+                    "VALUES " +
+                    "('Celsius');");
+            db.execSQL("INSERT INTO " + TABLE_TEMPERATURE + "(" + TEMPERATURE_NAME + ") " +
+                    "VALUES " +
+                    "('32.0F');");
+        }
+        catch (Exception e) {
+            Log.d("Error:", e.toString());
+            Toast.makeText(getApplicationContext(), "Database Issue: " + e.toString(), Toast
+                    .LENGTH_LONG).show();
+        }
+        finally { // This will always execute
+            Log.d("Data written:", " ");
+            db.close();
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
